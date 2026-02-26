3163cd14ae67e4c61104a80572ac2d9ca9b1bb3c
Shlok-Bhakta <shlok@spud>
2026-02-26 10:21:06 -0600

    nvim: switch competitest to split UI sidebar, add auto-run on save toggle

--- nvim/.config/nvim/lua/plugins/competitest.lua
+++ nvim/.config/nvim/lua/plugins/competitest.lua
@@ -9,6 +9,8 @@ return {
       { "<leader>Cr", "<cmd>CompetiTest run<cr>",            desc = "Run testcases" },
       { "<leader>CR", "<cmd>CompetiTest run_no_compile<cr>", desc = "Run (no compile)" },
       { "<leader>Cu", "<cmd>CompetiTest show_ui<cr>",        desc = "Show UI" },
+      -- watch mode toggle
+      { "<leader>Cw", desc = "Toggle auto-run on save" },
       -- testcase management
       { "<leader>Ca", "<cmd>CompetiTest add_testcase<cr>",    desc = "Add testcase" },
       { "<leader>Ce", "<cmd>CompetiTest edit_testcase<cr>",   desc = "Edit testcase" },
@@ -24,7 +26,23 @@ return {
       local cpp_dump_include = vim.fn.expand("~/dotfiles/cpp-dump")
       require("competitest").setup({
         runner_ui = {
-          interface = "popup",
+          interface = "split",
+        },
+        split_ui = {
+          position = "right",
+          relative_to_editor = true,
+          total_width = 0.3,
+          vertical_layout = {
+            { 1, "tc" },
+            { 1, { { 1, "so" }, { 1, "eo" } } },
+            { 1, { { 1, "si" }, { 1, "se" } } },
+          },
+          total_height = 0.4,
+          horizontal_layout = {
+            { 2, "tc" },
+            { 3, { { 1, "so" }, { 1, "si" } } },
+            { 3, { { 1, "eo" }, { 1, "se" } } },
+          },
         },
         compile_command = {
           c = { exec = "gcc", args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT)" } },
@@ -64,6 +82,34 @@ return {
         template_file = { cpp = vim.fn.expand("~/.config/nvim/templates/cp.cpp") },
         evaluate_template_modifiers = true,
       })
+
+      -- auto-run on save toggle
+      -- keyed by bufnr so each buffer tracks its own watch state
+      local watch_augroup = vim.api.nvim_create_augroup("CompetitestWatch", { clear = true })
+      local watching = {} -- bufnr -> true/false
+
+      vim.keymap.set("n", "<leader>Cw", function()
+        local buf = vim.api.nvim_get_current_buf()
+        if watching[buf] then
+          watching[buf] = false
+          -- remove just this buffer's autocmd by recreating the group
+          -- (we track per-buffer via the callback check above)
+          vim.notify("CompetiTest: stopped watching " .. vim.fn.expand("%:t"), vim.log.levels.INFO)
+        else
+          watching[buf] = true
+          vim.api.nvim_create_autocmd("BufWritePost", {
+            group = watch_augroup,
+            buffer = buf,
+            callback = function()
+              if watching[buf] then
+                vim.cmd("CompetiTest run")
+              end
+            end,
+            desc = "CompetiTest auto-run on save",
+          })
+          vim.notify("CompetiTest: watching " .. vim.fn.expand("%:t"), vim.log.levels.INFO)
+        end
+      end, { desc = "Toggle auto-run on save" })
     end,
   },
 }

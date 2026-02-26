b5f59a1752cce2a130441836e9453d3a5bd7b069
Shlok-Bhakta <shlok@spud>
2026-02-26 17:46:29 -0600

    update test runner to not suck

--- nvim/.config/nvim/lua/plugins/competitest.lua
+++ nvim/.config/nvim/lua/plugins/competitest.lua
@@ -3,28 +3,51 @@ return {
     "xeluxee/competitest.nvim",
     dependencies = "MunifTanjim/nui.nvim",
     keys = {
-      -- group label
-      { "<leader>C", group = "competitest" },
-      -- run
-      { "<leader>Cr", "<cmd>CompetiTest run<cr>",            desc = "Run testcases" },
-      { "<leader>CR", "<cmd>CompetiTest run_no_compile<cr>", desc = "Run (no compile)" },
-      { "<leader>Cu", "<cmd>CompetiTest show_ui<cr>",        desc = "Show UI" },
-      -- watch mode toggle
+      -- group label (also serves as lazy-load triggers)
+      { "<leader>C",  desc = "competitest" },
+      { "<leader>Cr", desc = "Run testcases" },
+      { "<leader>CR", desc = "Run (no compile)" },
+      { "<leader>Cu", desc = "Show UI" },
       { "<leader>Cw", desc = "Toggle auto-run on save" },
-      -- testcase management
-      { "<leader>Ca", "<cmd>CompetiTest add_testcase<cr>",    desc = "Add testcase" },
-      { "<leader>Ce", "<cmd>CompetiTest edit_testcase<cr>",   desc = "Edit testcase" },
-      { "<leader>Cd", "<cmd>CompetiTest delete_testcase<cr>", desc = "Delete testcase" },
-      -- receive (competitive-companion)
-      { "<leader>Ct", "<cmd>CompetiTest receive testcases<cr>",    desc = "Receive testcases" },
-      { "<leader>Cp", "<cmd>CompetiTest receive problem<cr>",      desc = "Receive problem" },
-      { "<leader>Cc", "<cmd>CompetiTest receive contest<cr>",      desc = "Receive contest" },
-      { "<leader>Cl", "<cmd>CompetiTest receive persistently<cr>", desc = "Receive persistently" },
-      { "<leader>Cs", "<cmd>CompetiTest receive stop<cr>",         desc = "Stop receiving" },
+      { "<leader>Ca", desc = "Add testcase" },
+      { "<leader>Ce", desc = "Edit testcase" },
+      { "<leader>Cd", desc = "Delete testcase" },
+      { "<leader>Ct", desc = "Receive testcases" },
+      { "<leader>Cp", desc = "Receive problem" },
+      { "<leader>Cc", desc = "Receive contest" },
+      { "<leader>Cl", desc = "Receive persistently" },
+      { "<leader>Cs", desc = "Stop receiving" },
     },
     config = function()
       local cpp_dump_include = vim.fn.expand("~/dotfiles/cpp-dump")
       local pch_file = vim.fn.expand("~/dotfiles/pch/stdc++.h.pch")
+
+      -- Returns the most recent normal (non-UI) buffer/window, so competitest
+      -- commands always operate on the source file even when focus is on the
+      -- split UI panel.
+      local function source_win()
+        for _, winid in ipairs(vim.api.nvim_list_wins()) do
+          local buf = vim.api.nvim_win_get_buf(winid)
+          if vim.bo[buf].buftype == "" and vim.bo[buf].filetype ~= "" then
+            return winid, buf
+          end
+        end
+        return vim.api.nvim_get_current_win(), vim.api.nvim_get_current_buf()
+      end
+
+      -- Temporarily focus the source window, run a command, then restore focus.
+      local function ct(cmd)
+        return function()
+          local cur_win = vim.api.nvim_get_current_win()
+          local src_win = source_win()
+          vim.api.nvim_set_current_win(src_win)
+          vim.cmd(cmd)
+          -- only restore if we didn't open a new UI (run/show_ui manage their own focus)
+          if src_win ~= cur_win and vim.api.nvim_win_is_valid(cur_win) then
+            vim.api.nvim_set_current_win(cur_win)
+          end
+        end
+      end
       require("competitest").setup({
         runner_ui = {
           interface = "split",
@@ -66,9 +89,9 @@ return {
           java = { exec = "javac", args = { "$(FABSPATH)" } },
         },
         run_command = {
-          c    = { exec = vim.fn.expand("~/dotfiles/run-and-clean.sh"), args = { "./$(FNOEXT).bin" } },
-          cpp  = { exec = vim.fn.expand("~/dotfiles/run-and-clean.sh"), args = { "./$(FNOEXT).bin" } },
-          rust = { exec = vim.fn.expand("~/dotfiles/run-and-clean.sh"), args = { "./$(FNOEXT).bin" } },
+          c    = { exec = vim.fn.expand("~/dotfiles/run-and-clean.sh"), args = { "$(ABSDIR)/$(FNOEXT).bin" } },
+          cpp  = { exec = vim.fn.expand("~/dotfiles/run-and-clean.sh"), args = { "$(ABSDIR)/$(FNOEXT).bin" } },
+          rust = { exec = vim.fn.expand("~/dotfiles/run-and-clean.sh"), args = { "$(ABSDIR)/$(FNOEXT).bin" } },
           python = { exec = "python", args = { "$(FNAME)" } },
           java = { exec = "java", args = { "$(FNOEXT)" } },
         },
@@ -86,6 +109,19 @@ return {
         evaluate_template_modifiers = true,
       })
 
+      -- keymaps: always operate on the source file window
+      vim.keymap.set("n", "<leader>Cr", ct("CompetiTest run"),            { desc = "Run testcases" })
+      vim.keymap.set("n", "<leader>CR", ct("CompetiTest run_no_compile"), { desc = "Run (no compile)" })
+      vim.keymap.set("n", "<leader>Cu", ct("CompetiTest show_ui"),        { desc = "Show UI" })
+      vim.keymap.set("n", "<leader>Ca", ct("CompetiTest add_testcase"),   { desc = "Add testcase" })
+      vim.keymap.set("n", "<leader>Ce", ct("CompetiTest edit_testcase"),  { desc = "Edit testcase" })
+      vim.keymap.set("n", "<leader>Cd", ct("CompetiTest delete_testcase"),{ desc = "Delete testcase" })
+      vim.keymap.set("n", "<leader>Ct", ct("CompetiTest receive testcases"),   { desc = "Receive testcases" })
+      vim.keymap.set("n", "<leader>Cp", ct("CompetiTest receive problem"),     { desc = "Receive problem" })
+      vim.keymap.set("n", "<leader>Cc", ct("CompetiTest receive contest"),     { desc = "Receive contest" })
+      vim.keymap.set("n", "<leader>Cl", ct("CompetiTest receive persistently"),{ desc = "Receive persistently" })
+      vim.keymap.set("n", "<leader>Cs", ct("CompetiTest receive stop"),        { desc = "Stop receiving" })
+
       -- auto-run on save toggle
       -- keyed by bufnr so each buffer tracks its own watch state
       local watch_augroup = vim.api.nvim_create_augroup("CompetitestWatch", { clear = true })
@@ -99,6 +135,9 @@ return {
           buffer = buf,
           callback = function()
             if watching[buf] then
+              -- always run against the source buffer, not whatever is focused
+              local src_win = source_win()
+              vim.api.nvim_set_current_win(src_win)
               vim.cmd("CompetiTest run")
             end
           end,
@@ -124,13 +163,13 @@ return {
       end
 
       vim.keymap.set("n", "<leader>Cw", function()
-        local buf = vim.api.nvim_get_current_buf()
+        local _, buf = source_win()
         if watching[buf] then
           watching[buf] = false
-          vim.notify("CompetiTest: stopped watching " .. vim.fn.expand("%:t"), vim.log.levels.INFO)
+          vim.notify("CompetiTest: stopped watching " .. vim.api.nvim_buf_get_name(buf), vim.log.levels.INFO)
         else
           enable_watch(buf)
-          vim.notify("CompetiTest: watching " .. vim.fn.expand("%:t"), vim.log.levels.INFO)
+          vim.notify("CompetiTest: watching " .. vim.api.nvim_buf_get_name(buf), vim.log.levels.INFO)
         end
       end, { desc = "Toggle auto-run on save" })
     end,
